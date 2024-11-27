0a3881e1f2e04e1edb01be5a501c7cc55a472914
Evan Hildreth <me@eph.me>
2024-11-27 09:52:58 -0500

    Add endpoints

--- public/index.php
+++ public/index.php
@@ -2,11 +2,16 @@
 
 namespace oddEvan\FetchEvaluation;
 
+use oddEvan\FetchEvaluation\Endpoints\GetPoints;
+use oddEvan\FetchEvaluation\Endpoints\ProcessReceipt;
+use oddEvan\FetchEvaluation\Rules\{ItemCount, ItemDescriptions, PurchaseDay, PurchaseTime, RetailerName, TotalAmount};
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Slim\Factory\AppFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$repo = new ReceiptRepo('sqlite:///' . __DIR__ . '/../data.sqlite');
 
 $app = AppFactory::create();
 
@@ -14,5 +19,25 @@ $app->get('/', function (RequestInterface $request, ResponseInterface $response)
 	$response->getBody()->write(\json_encode(['hello' => 'world', 'version' => '1.0']));
 	return $response;
 });
+
+$app->post(
+	'/receipts/process',
+	fn($request, $response) => (new ProcessReceipt(
+		repo: $repo,
+		calc: new PointCalculator([
+			new ItemCount(),
+			new ItemDescriptions(),
+			new PurchaseDay(),
+			new PurchaseTime(),
+			new RetailerName(),
+			new TotalAmount(),
+		])
+	))->run($request, $response)
+);
+
+$app->get(
+	'/receipts/{id}/points',
+	fn($request, $response, $arguments) => (new GetPoints(repo: $repo))->run($request, $response, $arguments)
+);
 
 $app->run();
